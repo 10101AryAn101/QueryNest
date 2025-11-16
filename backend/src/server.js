c6b179e32af1f2bd1593df1d88b6386f7ea26d4d
10101AryAn101 <aryanshukla9358@gmail.com>
2025-11-16 21:08:55 +0530

    changing

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const app = express()
 
 // CORS config — allow frontend
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://querynest-1-c6h4.onrender.com' ],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://querynest-1-aseq.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
